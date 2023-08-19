aa1f2e28ef294ab71e0170304ef74257f0cfbf12
= <=>
2023-08-19 18:08:55 -0400

    bread

--- react-bootstrap-app/src/App.js
+++ react-bootstrap-app/src/App.js
@@ -10,11 +10,11 @@ function App() {
           Hello! This is my (MeggalBozale)'s portfolio site. This site is built using React on a remote-hosted server.<br></br>
           This site is currently under construction, primarily in the design phase. Once construction is complete, <br></br>
           the site will host demos for a variety of projects I have worked on over the years, along with some posts of those <br></br>
-          which may not have demonstrations to use. 
-          <a href="https://github.com/MeggalBozale/Goonstation_Gizmos">Goonstation Gizmos</a>
+          which may not have demonstrations to use. <br></br>
+          <a href="https://github.com/MeggalBozale/Goonstation_Gizmos">Goonstation Gizmos</a><br></br><br></br>
 
-          I am currently a student entering college, looking to pursue my Bachelor's in Computer Science. I've been enthralled by
-          computers since I was young, and ever since I have spent my days tinkering, experimenting, and learning. I hope to show off
+          I am currently a student entering college, looking to pursue my Bachelor's in Computer Science. I've been enthralled by<br></br>
+          computers since I was young, and ever since I have spent my days tinkering, experimenting, and learning. I hope to show off<br></br>
           a bunch of cool stuff to the world over time, so stay tuned!
         </p>
       </header>
