cd7519b6b57d4053bdb4072341f32eb04f0407fa
= <=>
2023-08-19 18:07:32 -0400

    first commit

---
+++ react-bootstrap-app/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello! This is my (MeggalBozale)'s portfolio site. This site is built using React on a remote-hosted server.<br></br>
+          This site is currently under construction, primarily in the design phase. Once construction is complete, <br></br>
+          the site will host demos for a variety of projects I have worked on over the years, along with some posts of those <br></br>
+          which may not have demonstrations to use. 
+          <a href="https://github.com/MeggalBozale/Goonstation_Gizmos">Goonstation Gizmos</a>
+
+          I am currently a student entering college, looking to pursue my Bachelor's in Computer Science. I've been enthralled by
+          computers since I was young, and ever since I have spent my days tinkering, experimenting, and learning. I hope to show off
+          a bunch of cool stuff to the world over time, so stay tuned!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
